c0d745448ab0737e5677b1ab4a682b0cb4be6ac2
Alessandro Scavone <alex.scavone@hotmail.it>
2024-04-15 11:21:23 +0200

    funzione check lab 1

--- Laboratori/Lab1/es2/src/main.rs
+++ Laboratori/Lab1/es2/src/main.rs
@@ -71,6 +71,8 @@ impl Board {
                         'B' => data[i-1][j] = 1,    //cella piena
                         _ => {}
                     }
+
+                    j = j+1;
                 }
             }
             i = i + 1;
@@ -82,14 +84,60 @@ impl Board {
         board
     }
 
+
+    pub fn add_boat(self, boat: Boat, pos: (usize, usize)) -> Result<Board, Error> {
+
+        let i: usize = pos.0;
+        let j:usize = pos.1;
+
+        let mut remaining_boats = self.boats.clone();
+        let mut data = self.data.clone();
+
+        let boat_dim = match boat {
+            Boat::Vertical(size) | Boat::Horizontal(size) => size.clone()
+        };
+
+        match check_boat_validity(&self, &boat, pos) {
+            None => {},
+            Some(error) => {
+                match error {
+                    Error::BoatCount => println!("Errore: numero di boats di questa dimensione finite!"),
+                    Error::Overlap => println!("Errore: sovrapposizione di due boats!"),
+                    Error::OutOfBounds => println!("Errore: boat fuori dalla scacchiera!")
+                }
+
+                return Err(error);
+            }
+        }
+
+        match boat {
+            Boat::Vertical(usize) => {
+
+                for i in 0..boat_dim {  // inserisco la nave (in verticale)
+                    data[i][usize] = 1;
+                }
+
+            },
+
+            Boat::Horizontal(usize) => {
+
+                for j in 0..boat_dim {  // inserisco la nave (in orizzontale)
+                    data[usize][j] = 1;
+                }
+            }
+        }
+
+        return Err(Error::Overlap);
+
+    }
+
+
     /*
     /* aggiunge la nave alla board, restituendo la nuova board se
     possibile */
     /* bonus: provare a *non copiare* data quando si crea e restituisce
     una nuova board con la barca, come si può fare? */
-    pub fn add_boat(self, boat: Boat, pos: (usize, usize)) -> Result<Board, Error> {
 
-    }
 
     /* converte la board in una stringa salvabile su file */
     pub fn to_string(&self) -> String {
@@ -97,7 +145,37 @@ impl Board {
     }*/
 }
 
+fn check_boat_validity(board: &Board, boat: &Boat, pos: (usize, usize)) -> Option<Error> {
 
+    let boat_dim = match boat {
+        Boat::Vertical(size) | Boat::Horizontal(size) => size.clone()
+    };
+
+    if board.boats[boat_dim-1] == 0 { return Some(Error::BoatCount); }
+    else if pos.0 < 0 || pos.0 > 19 || pos.1 < 0 || pos.1 > 19 {return Some(Error::OutOfBounds);}
+    else {
+        match boat {
+            Boat::Horizontal(size) => {
+                if boat_dim + pos.1 -1 > 19 {return Some(Error::OutOfBounds);}
+
+                for j in 0..boat_dim {
+                    if board.data[pos.0][pos.1 + j] == 1 {return Some(Error::Overlap);}
+                }
+            }
+            Boat::Vertical(size) => {
+                if boat_dim + pos.0 -1 > 19 {return Some(Error::OutOfBounds);}
+
+                for i in 0..boat_dim {
+                    if board.data[pos.0 + i][pos.1] == 1 {return Some(Error::Overlap);}
+                }
+            }
+        }
+    }
+
+
+
+    return None;
+}
 
 fn main() {
 
@@ -105,6 +183,7 @@ fn main() {
 
 
     let prova = Board::from(fs::read_to_string(path).unwrap());
+
 
     println!("fine");
 }
