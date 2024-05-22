d1701f28fb495b68a543b30b3430b8de0b30918d
Alessandro Scavone <s328782@studenti.polito.it>
2024-05-22 18:55:09 +0200

    Inizio lab4

--- Laboratori/Lab1/prove/src/main.rs
+++ Laboratori/Lab1/prove/src/main.rs
@@ -1,88 +1,29 @@
-use std::time::SystemTime;
+use std::cmp::Ordering;
+use std::collections::BTreeMap;
 
-enum Error{
-    Simple {time: SystemTime},
-    Complex {time: SystemTime, err: String}
-}
+impl Ord for BTreeMap<i32, &str> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self < other {
+            return Ordering::Greater;
+        }
 
-enum MulErr {
-    Overflow,
-    NegativeNumber
-}
-
-struct Node {
-    name: String,
-    size: u32,
-    count: u32,
-}
-
-impl Node {
-    pub fn new(n: String) -> Node {
-        Node {name: n, size: 0, count: 0}
+        return Ordering::Less;
     }
-
-    pub fn size(&mut self, size: u32) {
-        self.size = size;
-    }
-
-    pub fn count_p(&mut self, count: u32) {
-        self.count = count;
-    }
-
-    pub fn to_string(&self) -> String {
-
-        format!("name: {}, size: {}, count: {}", self.name, self.size, self.count)
-    }
-
-}
-
-fn print_error(e: Error){
-    
-    match e {
-        Error::Simple { time } => println!("time = {:?}", time),
-        Error::Complex { time , err } => println!("time = {:?}, error = {}", time, err)
-    }
-}
-
-fn mul(a: i32, b: i32) -> Result<u32, MulErr> {
-
-    if a < 0 || b < 0 {
-        return Err(MulErr::NegativeNumber)
-    }
-    else if a.overflowing_mul(b).1 == true  {
-        return Err(MulErr::Overflow)
-    }
-
-    let ris = a as u32 * b as u32;
-
-    Ok(ris)
 }
 
 fn main() {
+    let mut map = BTreeMap::new();
+    map.insert(3, "tre");
+    map.insert(1, "uno");
+    map.insert(4, "quattro");
+    map.insert(2, "due");
+    map.insert(5, "cinque");
 
-    let error = Error::Complex {time: SystemTime::now(), err: "Suca".to_string()};
+    println!("Mappa: {:?}", map);
 
-    print_error(error);
-
-    let risultato = mul(-29, 10);
-
-    match risultato {
-        Ok(value) => {println!("{}", value)},
-        Err(error) => {
-            match error {
-                MulErr::Overflow {} => println!("Overflow!"),
-                MulErr::NegativeNumber {} => println!("Numero Negativo :(")
-            }
-        }
+    // Iterazione sugli elementi della mappa
+    println!("Iterazione sulla mappa:");
+    for (key, value) in &map {
+        println!("Chiave: {}, Valore: {}", key, value);
     }
-
-
-    let mut node: Node =  Node::new("node".to_string());
-
-    node.size(5);
-    node.count_p(4);
-
-
-    println!("{}", node.to_string());
 }
-
---
+++ Laboratori/Lab4/es1/target/debug/deps/es1.d
@@ -0,0 +1,6 @@
+C:\Users\Utente\Desktop\poli\materie\Programmazione di Sistema\Rust\Laboratori\Lab4\es1\target\debug\deps\es1.exe: src\main.rs src\es0401.rs
+
+C:\Users\Utente\Desktop\poli\materie\Programmazione di Sistema\Rust\Laboratori\Lab4\es1\target\debug\deps\es1.d: src\main.rs src\es0401.rs
+
+src\main.rs:
+src\es0401.rs:
